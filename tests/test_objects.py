2da08cc6b8c729be54fe959c56e0f3ddebd73bec
Rob Huneault <3034994+rhuneault@users.noreply.github.com>
2020-04-22 14:15:50 -0400

    Modified folder structure. Tests don't need an __init__.

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -7,13 +7,15 @@ class TestApplication(unittest.TestCase):
     """
     """
 
-    def test_application_list(self):
+    def test_application_properties(self):
         """
         """
         a = Application('TD Canada Trust', 'Senior Confabulator', 'Not applied')
         assert a.company == 'TD Canada Trust'
         assert a.position == 'Senior Confabulator'
         assert a.status == 'Not applied'
+
+
 
 
 if __name__ == '__main__':
