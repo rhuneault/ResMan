7ece5beaa8807cead9f1cc4b9451a42bab7937b3
Rob Huneault <3034994+rhuneault@users.noreply.github.com>
2020-04-23 13:40:38 -0400

    Small formatting changes

--- app/objs/objects.py
+++ app/objs/objects.py
@@ -7,11 +7,9 @@ This module contains key application objects for the Resume Manager application.
 class Application(object):
     """A job application.
     """
-
     def __init__(self, company_name, position_title, status):
         """Initialize a new Application object.
         """
-
         self._company = company_name
         self._position = position_title
         self._status = status
@@ -20,19 +18,16 @@ class Application(object):
     def company(self):
         """The name of the company which is offering the job.
         """
-
         return self._company
 
     @property
     def position(self):
         """The position which is being offered within the company.
         """
-
         return self._position
 
     @property
     def status(self):
         """The status of the job application.
         """
-
         return self._status
