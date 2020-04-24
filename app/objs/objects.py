be6f2d72c45957eea0b10e810dd531fae6c0abdd
Rob Huneault <3034994+rhuneault@users.noreply.github.com>
2020-04-23 22:24:18 -0400

    Added a name and name output to the Resume class.

--- app/objs/objects.py
+++ app/objs/objects.py
@@ -31,3 +31,50 @@ class Application(object):
         """The status of the job application.
         """
         return self._status
+
+
+class ResumeBase(object):
+    """A job resume.
+    """
+    def __init__(self):
+        """
+        """
+        self._first = None
+        self._middle = None
+        self._last = None
+
+    def add_name(self, first, last, middle=None):
+        """Add the job applicant's name to the resume.
+
+        :param first: The first name of the job applicant
+        :param last: The last name of the job applicant
+        :return: None
+        """
+        self._first = first
+        self._last = last
+        self._middle = middle
+
+    def name(self, middle=None):
+        """Return the name in the requested format.
+
+        :return: The first and last name of the job applicant
+        """
+        if self._first is None or self._last is None:
+            raise ValueError('First and last name are required.')
+
+        if middle:
+            if middle.lower() == 'omit':
+                return self._first.title() + ' ' + self._last.title()
+            elif middle.lower() == 'initial':
+                if self._middle is None:
+                    raise ValueError('Middle initial format requested, but no middle name provided.')
+                return self._first.title() + ' ' + self._middle[0].upper() + '. ' + self._last.title()
+            # else:
+            #     raise
+        else:
+            if self._middle:
+                return self._first.title() + ' ' + self._middle.title() + ' ' + self._last.title()
+            else:
+                return self._first.title() + ' ' + self._last.title()
+
+
--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,6 +1,6 @@
 import unittest
 
-from app.objs.objects import Application
+from app.objs.objects import Application, ResumeBase
 
 
 # coverage run --source=app -m nose2
@@ -21,10 +21,48 @@ class TestApplication(unittest.TestCase):
 class TestResume(unittest.TestCase):
     """
     """
-    def test_resume_properties(self):
+    def test_resume_name_raise_error(self):
         """
         """
+        rb = ResumeBase()
+        with self.assertRaises(ValueError):
+            print(rb.name())
 
+    def test_resume_name(self):
+        """
+        """
+        rb = ResumeBase()
+        rb.add_name('John', 'Doe')
+        self.assertEqual(rb.name(), 'John Doe')
+
+    def test_resume_middle_name(self):
+        """
+        """
+        rb = ResumeBase()
+        rb.add_name('John', 'Doe', middle='Patrick')
+        self.assertEqual(rb.name(), 'John Patrick Doe')
+
+    def test_resume_no_middle_name(self):
+        """
+        """
+        rb = ResumeBase()
+        rb.add_name('John', 'Doe', middle='Patrick')
+        self.assertEqual(rb.name(middle='omit'), 'John Doe')
+
+    def test_resume_middle_initial_name(self):
+        """
+        """
+        rb = ResumeBase()
+        rb.add_name('John', 'Doe', middle='Patrick')
+        self.assertEqual(rb.name(middle='initial'), 'John P. Doe')
+
+    def test_resume_no_middle_name(self):
+        """
+        """
+        rb = ResumeBase()
+        rb.add_name('John', 'Doe')
+        with self.assertRaises(ValueError):
+            print(rb.name(middle='initial'))
 
 
 if __name__ == '__main__':
